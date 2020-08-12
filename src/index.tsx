b9ba9806f6837d224f341ea1bd6f8cab24da6d0a
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-08-12 16:34:07 +0900

    2-2. arrange visual

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 function App() {
-  return <h1>Hello React!</h1>
+  return (
+    <div>
+      <header>
+        <div>Kanban Board</div>
+        <input placeholder="Filter cards"/>
+      </header>
+      <div>
+        <section>
+          <h3>ToDo</h3>
+          <article>朝食を取る</article>
+          <article>SNSをチェックする</article>
+          <article>布団に入る</article>
+        </section>
+        <section>
+          <h3>Doing</h3>
+          <article>顔を洗う</article>
+          <article>歯を磨く</article>
+        </section>
+        <section>
+          <h3>Waiting</h3>
+        </section>
+        <section>
+          <h3>Done</h3>
+          <article>布団から出る</article>
+        </section>
+      </div>
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById('app'))
