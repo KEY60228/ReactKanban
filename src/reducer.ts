6859d9c8d70524b5f553c697637320e3e4fb354d
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-08-20 22:22:28 +0900

    5-2. setColumns and setCards mock

--- src/reducer.ts
+++ src/reducer.ts
@@ -1,12 +1,24 @@
 import { Reducer } from 'redux'
 import produce from 'immer'
+import { CardID, ColumnID } from './api'
 
 export type State = {
   filterValue: string
+  columns?: {
+    id: ColumnID
+    title?: string
+    text?: string
+    cards?: {
+      id: CardID
+      text?: string
+    }[]
+  }[]
+  cardsOrder: Record<string, CardID | ColumnID | null>
 }
 
 const initialState: State = {
-  filterValue: ''
+  filterValue: '',
+  cardsOrder: {},
 }
 
 export type Action = {
@@ -14,6 +26,24 @@ export type Action = {
   payload: {
     value,
   },
+} | {
+  type: 'App.SetColumns'
+  payload: {
+    columns: {
+      id: ColumnID
+      title?: string
+      text?: string
+    }[]
+  }
+} | {
+  type: 'App.SetCards'
+  payload: {
+    cards: {
+      id: CardID
+      text?: string
+    }[]
+    cardsOrder: Record<string, CardID | ColumnID | null>
+  }
 }
 
 export const reducer: Reducer<
@@ -25,6 +55,18 @@ export const reducer: Reducer<
       const { value } = action.payload
       draft.filterValue = value
       return
+    }
+
+    case 'App.SetColumns': {
+      return
+    }
+
+    case 'App.SetCards': {
+      return
+    }
+
+    default: {
+      const _: never = action
     }
   }
 }, initialState)
